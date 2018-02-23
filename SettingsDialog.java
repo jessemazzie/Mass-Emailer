3dd96d3fcf820a39a6422e2d9b83d3cb03841aba
Jesse Mazzie <jessegalt@yahoo.com>
2018-02-22 20:27:42 -0500

    Added JPanel and JTextFields/JPasswordFields.

--- SettingsDialog.java
+++ SettingsDialog.java
@@ -3,8 +3,42 @@ import javax.swing.*;
 
 public class SettingsDialog extends JDialog {
 	Container cp;
+	JTextField serverDomainBox;
+	JTextField portNumberBox;
+	JTextField senderUsername;
+	JPasswordField senderPassword;
+	JTextField sentFrom;
+	JTextField sentDate;
+	JTextField subject;
+	
 	
 	SettingsDialog() {
+		JPanel inputPanel = new JPanel(new GridLayout(7, 2));
+		
+		//Instantiate our textfields/passwordfields
+		serverDomainBox = new JTextField(15);
+		portNumberBox = new JTextField(5);
+		senderUsername = new JTextField(15);
+		senderPassword = new JPasswordField(15);
+		sentFrom = new JTextField(15);
+		sentDate = new JTextField(15);
+		subject = new JTextField(15);
+		
+		//Add them to the JPanel
+		inputPanel.add(new JLabel("Server domain: "));
+		inputPanel.add(serverDomainBox);
+		inputPanel.add(new JLabel("Port number: "));
+		inputPanel.add(portNumberBox);
+		inputPanel.add(new JLabel("Sender username: "));
+		inputPanel.add(senderUsername);
+		inputPanel.add(new JLabel("Sender password: "));
+		inputPanel.add(senderPassword);
+		inputPanel.add(new JLabel("Sent from: "));
+		inputPanel.add(sentFrom);
+		inputPanel.add(new JLabel("Sent date: "));
+		inputPanel.add(sentDate);
+		inputPanel.add(new JLabel("Subject: "));
+		inputPanel.add(subject);
 		
 		
 		//configure the dialog
@@ -12,5 +46,7 @@ public class SettingsDialog extends JDialog {
 		setTitle("Mass Emailer");
 		setVisible(true);
 		setSize(500, 500);
+		
+		cp.add(inputPanel);
 	}
 }
